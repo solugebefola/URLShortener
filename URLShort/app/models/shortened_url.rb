c3c449e30e13b0542cadaed0aabc786814cc4d57
Solar Olugebefola <s.olugebefola@gmail.com>
2015-10-15 01:09:31 -0400

    Refactor ShortenedUrl class for clarity, shorten #num_clicks, refactor bin/cli to record visits.

--- URLShort/app/models/shortened_url.rb
+++ URLShort/app/models/shortened_url.rb
@@ -6,16 +6,16 @@ class ShortenedUrl < ActiveRecord::Base
 
   belongs_to(
     :submitter,
-    :class_name =>  'User',
-    :foreign_key => :submitter_id,
-    :primary_key => :id
+    class_name:   'User',
+    foreign_key:  :submitter_id,
+    primary_key:  :id
     )
 
   has_many(
     :visits,
-    class_name: 'Visit',
-    foreign_key: :shortened_url_id,
-    primary_key: :id
+    class_name:   'Visit',
+    foreign_key:  :shortened_url_id,
+    primary_key:  :id
   )
 
   has_many(
@@ -37,24 +37,27 @@ class ShortenedUrl < ActiveRecord::Base
 
   def self.create_for_user_and_long_url!(user, long_url)
     new_random_code = self.random_code.to_s
-    self.create!(submitter_id: user.id, long_url: long_url, short_url: new_random_code)
+    self.create!(
+      submitter_id: user.id,
+      long_url: long_url,
+      short_url: new_random_code
+    )
 
     new_random_code
   end
 
   def num_clicks
-    Visit.select(:visitor_id).where("shortened_url_id = ?", self.id).count
+    Visit.where("shortened_url_id = ?", self.id).count
   end
 
   def num_uniques
     visitors.count
-    #Visit.select(:visitor_id).where("shortened_url_id = ?", self.id).distinct.count
   end
 
   def num_recent_uniques
-    # visitors.where("created_at > ?", 10.minutes.ago).count
-
-    Visit.select(:visitor_id).where("created_at > ? AND shortened_url_id = ?", 10.minutes.ago, self.id).distinct.count
+    Visit.select(:visitor_id).where(
+    "created_at > ? AND shortened_url_id = ?", 10.minutes.ago, self.id
+    ).distinct.count
   end
 
 end
